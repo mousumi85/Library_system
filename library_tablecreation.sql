c03878ee8d04047577ed574716a3162948dbf9b7
mousumi85 <155497062+mousumi85@users.noreply.github.com>
2024-01-02 19:13:13 -0500

    Add files via upload

---
+++ library_tablecreation.sql
@@ -0,0 +1,14 @@
+create table student1 (library_id int primary key,name varchar(20),address varchar(20),ph_no int,
+					  email_id varchar(20),branch varchar(20),no_of_books_issued int)
+					  
+
+
+create table admin(admin_id int primary key,name varchar(20),ph_no int)
+
+create table log_in(log_in_id varchar(20),passwd varchar(20))
+
+create table book(title varchar(20),author varchar(20),barcode_no int,
+				  price float,status varchar(20))
+				  
+				  
+create table log(issue_date date,return_date date,due_date date,fine int,student_id int)
